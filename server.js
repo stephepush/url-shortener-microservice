430573aa83c43dbe0d77091021102bd85918b7dc
Stephen <7speter@gmail.com>
2022-01-03 00:19:45 -0500

    Experiments with dns module and dns.lookup

--- server.js
+++ server.js
@@ -2,13 +2,17 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dns = require('dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+app.use(express.urlencoded({extended:true}));
+app.use(express.json())
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -18,6 +22,18 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.post('/api/shorturl/', function(req, res){
+  console.log(req.body)
+  dns.lookup('apple.com', (err, address, family)=>{
+    console.log(`address: ${address} family: IPv${family}`)
+  })
+  res.end()
+})
+
+app.get('/api/shorturl/:id', function(req,res){
+
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
