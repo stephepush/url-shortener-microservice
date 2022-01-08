ea53f99ee7daa8ff9c52aa405fd213fc9fe1f51e
Stephen <7speter@gmail.com>
2022-01-07 19:59:08 -0500

    Moves static method incrementId below save method in shortenedLink class file

--- models/shortenedLink.js
+++ models/shortenedLink.js
@@ -1,45 +1,46 @@
 const fs = require('fs');
 const path = require('path');
 
+
 module.exports = class ShortenedLink {
 
-    constructor(url, cutUrl){
+    constructor(url, cutUrl) {
         this.id = ShortenedLink.incrementId()
         this.url = url;
         this.cutUrl = cutUrl
 
     }
 
-    static incrementId() {
-        if(!this.latestId) this.latestId = 1
-        else this.latestId++
-        return this.latestId
-    }
-
-    save(){
+    save() {
         const p = path.join(
             path.dirname(require.main.filename),
             'data',
             'shortenedLinks.json'
         );
-        fs.readFile(p, (err, fileContent) =>{
+        fs.readFile(p, (err, fileContent) => {
             let links = []
             if (!err) {
                 links = JSON.parse(fileContent)
             }
             links.push(this)
-            fs.writeFile(p, JSON.stringify(links), (err)=>{console.log(err)})
+            fs.writeFile(p, JSON.stringify(links), (err) => { console.log(err) })
         })
     }
 
-    static fetchAll(){
+    static incrementId() {
+        if (!this.latestId) this.latestId = 1
+        else this.latestId++
+            return this.latestId
+    }
+
+    static fetchAll() {
         const p = path.join(
             path.dirname(require.main.filename),
             'data',
             'shortenedLinks.json'
         );
-        fs.readFile(p, (err,fileContent)=>{
-            if (err){
+        fs.readFile(p, (err, fileContent) => {
+            if (err) {
                 return [];
             }
             return JSON.parse(fileContent)
