4a7ce2dcc8b5ada7c2659c4d22387f976e745161
Stephen <7speter@gmail.com>
2022-01-07 19:09:28 -0500

    Creates shortenedLink model, adds create and fetchall methods there

--- models/shortenedLink.js
+++ models/shortenedLink.js
@@ -1,16 +1,49 @@
-const links = []
+const fs = require('fs');
+const path = require('path');
 
 module.exports = class ShortenedLink {
-    constructor(id, url ){
-        this.id = id;
+
+    constructor(url, cutUrl){
+        this.id = ShortenedLink.incrementId()
         this.url = url;
+        this.cutUrl = cutUrl
+
+    }
+
+    static incrementId() {
+        if(!this.latestId) this.latestId = 1
+        else this.latestId++
+        return this.latestId
     }
 
     save(){
-        links.push(this)
+        const p = path.join(
+            path.dirname(require.main.filename),
+            'data',
+            'shortenedLinks.json'
+        );
+        fs.readFile(p, (err, fileContent) =>{
+            let links = []
+            if (!err) {
+                links = JSON.parse(fileContent)
+            }
+            links.push(this)
+            fs.writeFile(p, JSON.stringify(links), (err)=>{console.log(err)})
+        })
     }
 
     static fetchAll(){
-        return this.links
+        const p = path.join(
+            path.dirname(require.main.filename),
+            'data',
+            'shortenedLinks.json'
+        );
+        fs.readFile(p, (err,fileContent)=>{
+            if (err){
+                return [];
+            }
+            return JSON.parse(fileContent)
+        })
+        return links
     }
 }
